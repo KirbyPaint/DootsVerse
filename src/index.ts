a81862ef4429b8ec59b315f2cbb4ac3a296457b9
Ash Porter <porter.ashley13@gmail.com>
2023-03-27 20:33:46 -0700

    bot restructure

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
-// Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require("discord.js");
-const { token } = require("../config.json");
+import "./config";
+import Bot from "./bot";
 
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
-});
-
-// Log in to Discord with your client's token
-client.login(token);
+void (async () => {
+  try {
+    await Bot.init();
+  } catch (e) {
+    console.error(e);
+    process.exit(-1);
+  }
+})();
